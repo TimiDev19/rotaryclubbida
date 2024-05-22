9d90eeb4dba069d02b8a100af81179713f9bf994
TimiDev19 <rotimioluwatimilehin19@gmail.com>
2024-05-22 08:33:22 -0700

    second round of changes made

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -11,7 +11,8 @@ const Navbar = () => {
   };
 
   return (
-    <nav className="fixed top-0 left-0 w-full z-50 bg-white backdrop-filter backdrop-blur-lg bg-opacity-30">
+    <nav className="fixed top-0 left-0 w-full z-50 bg-white">
+      {/* backdrop-filter backdrop-blur-lg bg-opacity-30 */}
       <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
         <div className="flex items-center justify-between h-16">
           <div className="flex items-center">
--- src/pages/About.js
+++ src/pages/About.js
@@ -35,9 +35,9 @@ function About() {
 
 
                 <div className='flex flex-row justify-between items-center w-full my-11'>
-                    <div className='bg-blue-900 w-5/12 p-8 rounded-2xl text-left'>
-                        <h1 className='text-amber-500 font-bold uppercase text-2xl mb-1 flex items-center justify-start'><img src={transparentLogo} className='w-1/4 mr-3' />Vision statement</h1>
-                        <p className='text-white'>
+                    <div className='bg-blue-200 w-5/12 p-8 rounded-2xl text-left'>
+                        <h1 className='text-amber-500 font-bold uppercase text-2xl mb-1 flex items-start flex-col justify-start'><img src={transparentLogo} className='w-1/4 mr-3' />Vision statement</h1>
+                        <p className='text-blue-900'>
                             Together we see a world where people unite and take action to create lasting change - across the globe, in our communities, and in ourselves.
                         </p>
                     </div>
@@ -68,9 +68,9 @@ function About() {
                     </div> */}
 
 
-                    <div className='bg-blue-900 w-5/12 p-8 rounded-2xl text-left'>
-                        <h1 className='text-amber-500 font-bold uppercase text-2xl mb-1 flex items-center justify-start'><img src={transparentLogo} className='w-1/4 mr-3' />core values</h1>
-                        <p className='text-white'>
+                    <div className='bg-blue-200 w-5/12 p-8 rounded-2xl text-left'>
+                        <h1 className='text-amber-500 font-bold uppercase text-2xl mb-1 flex flex-col items-start justify-start'><img src={transparentLogo} className='w-1/4 mr-3' />core values</h1>
+                        <p className='text-blue-900'>
                             Our core values are Service, Fellowship, Leadership, Integrity, Diversity
                         </p>
                     </div>
@@ -78,15 +78,15 @@ function About() {
 
 
 
-                <div className='bg-blue-900 w-3/5 m-auto p-8 rounded-2xl text-left'>
+                <div className='bg-blue-200 w-3/5 m-auto p-8 rounded-2xl text-left'>
                     <h1 className='text-amber-500 font-bold uppercase text-2xl mb-1'>object of rotary</h1>
-                    <p className='text-white mb-5'>
+                    <p className='text-blue-900 mb-5'>
                         The Object of Rotary is to encourage and foster the ideal of service as a basis of worthy enterprise and, in particular, to encourage and foster:
                     </p>
-                    <p className='font-semibold text-amber-500'><span className='text-white uppercase'>First: </span>The development of acquaintance as an opportunity for service</p>
-                    <p className='font-semibold text-amber-500'><span className='text-white uppercase'>Second: </span>High ethical standards in business and professions; the recognition of the worthiness of all useful occupations; and the dignifying of each Rotarian’s occupation as an opportunity to serve society</p>
-                    <p className='font-semibold text-amber-500'><span className='text-white uppercase'>Third: </span>The application of the ideal of service in each Rotarian’s personal, business, and community life</p>
-                    <p className='font-semibold text-amber-500'><span className='text-white uppercase'>Fourth: </span>The advancement of international understanding, goodwill, and peace through a world fellowship of business and professional persons united in the ideal of service.</p>
+                    <p className='font-semibold text-amber-500'><span className='text-blue-900 uppercase'>First: </span>The development of acquaintance as an opportunity for service</p>
+                    <p className='font-semibold text-amber-500'><span className='text-blue-900 uppercase'>Second: </span>High ethical standards in business and professions; the recognition of the worthiness of all useful occupations; and the dignifying of each Rotarian’s occupation as an opportunity to serve society</p>
+                    <p className='font-semibold text-amber-500'><span className='text-blue-900 uppercase'>Third: </span>The application of the ideal of service in each Rotarian’s personal, business, and community life</p>
+                    <p className='font-semibold text-amber-500'><span className='text-blue-900 uppercase'>Fourth: </span>The advancement of international understanding, goodwill, and peace through a world fellowship of business and professional persons united in the ideal of service.</p>
                 </div>
 
 
@@ -100,9 +100,9 @@ function About() {
                         </p>
                     </div> */}
 
-                    <div className='bg-blue-900 w-2/5 p-8 rounded-2xl text-left'>
+                    <div className='bg-blue-200 w-2/5 p-8 rounded-2xl text-left'>
                         <h1 className='text-amber-500 font-bold uppercase text-2xl mb-1'>The four way test</h1>
-                        <p className='text-white mb-5'>
+                        <p className='text-blue-900 mb-5'>
                             The Four-Way Test is a nonpartisan and nonsectarian ethical guide for Rotarians to use for their personal and professional relationships. The test has been translated into more than 100 languages, and Rotarians recite it at club meetings:
                             Of the things we think, say or do
                         </p>
@@ -114,9 +114,9 @@ function About() {
 
 
 
-                    <div className='bg-blue-900 w-2/5 p-8 rounded-2xl text-left'>
+                    <div className='bg-blue-200 w-2/5 p-8 rounded-2xl text-left'>
                         <h1 className='text-amber-500 font-bold uppercase text-2xl mb-1'>Areas Of Focus</h1>
-                        <p className='text-white mb-5'>We direct our efforts to enhance our local and global impact. Our most successful and sustainable projects and activities tend to fall within the following areas:</p>
+                        <p className='text-blue-900 mb-5'>We direct our efforts to enhance our local and global impact. Our most successful and sustainable projects and activities tend to fall within the following areas:</p>
                         <p className='font-semibold text-amber-500'>Promoting peace</p>
                         <p className='font-semibold text-amber-500'>Fighting Disease</p>
                         <p className='font-semibold text-amber-500'>Providing clean water</p>
